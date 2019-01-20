6f602ed04e4c58dcc01cec1d1df1f11ee0de6e8d
Gui Talarico <gtalarico@users.noreply.github.com>
2019-01-19 22:57:03 -0800

    Update pm.rb
    Fixed url

--- Formula/pm.rb
+++ Formula/pm.rb
@@ -1,7 +1,7 @@
 class Pm < Formula
   desc "Minimal Project CLI Manager"
   homepage "https://github.com/gtalarico/pm"
-  url "http://github.com/gtalarico/pm/releases/v0.1.0/pm_0.1.0_Darwin_x86_64.tar.gz"
+  url "http://github.com/gtalarico/pm/releases/download/v0.1.0/pm_0.1.0_Darwin_x86_64.tar.gz"
   version "0.1.0"
   sha256 "e182fd31f10782c6c0a6a86e16ff020f965cd6ba4e2df50b95c341f6b9ea5453"
 
