d21561a887b2f912242445e63567cf9be765e9d8
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-25 02:00:44 -0800

    Brew formula update for pm version v0.1.6

--- Formula/pm.rb
+++ Formula/pm.rb
@@ -1,9 +1,9 @@
 class Pm < Formula
   desc "Minimal Project CLI Manager"
   homepage "https://github.com/gtalarico/pm"
-  url "http://github.com/gtalarico/pm/releases/download/v0.1.4/pm_0.1.4_Darwin_x86_64.tar.gz"
-  version "0.1.4"
-  sha256 "039776082135446a072aae66a48f601a49794cb1ccb32969abe586318910cf96"
+  url "http://github.com/gtalarico/pm/releases/download/v0.1.6/pm_0.1.6_Darwin_x86_64.tar.gz"
+  version "0.1.6"
+  sha256 "962fb2c28056ce4028c678425781ba0d2c87bf106571dca632ff397d33dfe392"
 
   def install
     bin.install "pm"
